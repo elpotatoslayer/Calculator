b3f6cd4449b872fb3b094313ab1142bcb6da6502
Ralph-tech <58685004+Ralph-tech@users.noreply.github.com>
2021-08-13 22:29:49 +0300

    Add state to app componete

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,34 @@
 /* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
 import './App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="app">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick(buttonName) {
+    const newState = calculate(this.state, buttonName);
+    this.setState(newState);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
