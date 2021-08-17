72efcc5d3a2b023e0760c07a60c682b523f929f3
Ralph-tech <58685004+Ralph-tech@users.noreply.github.com>
2021-08-17 11:49:58 +0300

    Refactor Calculator

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,23 @@
 import PropTypes from 'prop-types';
 import './button.css';
 
-export default function Button({ buttonName, clickHandler }) {
-  return (
-    <button
-      type="button"
-      className="button"
-      onClick={() => clickHandler({ buttonName })}
-      onContextMenu={(e) => {
-        e.preventDefault();
-        clickHandler(buttonName);
-      }}
-    >
-      {buttonName}
-    </button>
-  );
-}
+const Button = ({ buttonName, clickHandler }) => (
+  <button
+    type="button"
+    className="button"
+    onClick={() => clickHandler({ buttonName })}
+    onContextMenu={(e) => {
+      e.preventDefault();
+      clickHandler(buttonName);
+    }}
+  >
+    {buttonName}
+  </button>
+);
 
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
   clickHandler: PropTypes.func.isRequired,
 };
+
+export default Button;
