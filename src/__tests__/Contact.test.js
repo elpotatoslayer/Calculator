41788d5ddc3b026990e5212d75f9a7bfe344e6e9
Ralph <ralphoburu@gmail.com>
2021-09-19 17:25:56 +0300

    Add contact test

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -1,12 +1,18 @@
 import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Quote from '../components/Quote';
+import Home from '../components/Home';
 
 describe('Home component test', () => {
+  test('should renders welcome text', () => {
+    render(<Home />);
+    const text = screen.getByText('Welcome to our page!');
+    expect(text).toBeInTheDocument();
+  });
 
   test('should render the expected output', () => {
     const component = renderer.create(
-      <Quote />,
+      <Home />,
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
